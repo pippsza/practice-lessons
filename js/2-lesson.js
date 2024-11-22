2365a6ff7d7475857f16e11194a6dd305a134ea8
Daria Golovashova <your_email>
2024-11-22 20:03:34 +0100

    task3

--- js/2-lesson.js
+++ js/2-lesson.js
@@ -13,21 +13,42 @@
 //яка приймає довільну кількість
 //аргументів і повертає їхнє середнє значення.
 //Додати перевірку, що аргументи це числа.
-const arr = [2, 6, 24, 7, 35, 24, "hello", 9, "world"];
+// const arr = [2, 6, 24, 7, 35, 24, "hello", 9, "world"];
 
-function caclculateAverage(arr) {
-  let sumIteration = 0;
-  let sum = 0;
+// function caclculateAverage(arr) {
+//   let sumIteration = 0;
+//   let sum = 0;
 
-  for (const arg of arr) {
-    if (typeof arg === "number") {
+//   for (const arg of arr) {
+//     if (typeof arg === "number") {
 
-      console.log(arg);
-      sum += arg;
-      sumIteration += 1;
+//       console.log(arg);
+//       sum += arg;
+//       sumIteration += 1;
+//     }
+//   }
+//   return sum / sumIteration;
+// }
+
+// console.log(caclculateAverage(arr));
+
+
+//Напиши функцію findLongestWord(string)
+//яка приймає довільний рядок
+//що складається лише з розділених слів
+//пробілом (параметр string)
+//і повертає найдовше слово у цьому рядку
+
+// const string = "Nunc sed turpis a felis in nunc fringilla"
+
+function findLongestWord(string) { 
+  const words = string.split(" ");
+  let longestWords = "";
+  for (let word of words) { 
+    if (word.length > longestWords.length) { 
+      longestWords = word;
     }
-  }
-  return sum / sumIteration;
+  } return longestWords;
 }
-
-console.log(caclculateAverage(arr));
+const string = "Nunc sed turpis a felis in nunc fringilla";
+console.log(findLongestWord(string));
