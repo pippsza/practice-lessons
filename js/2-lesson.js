7c31427ca1b336782a0ce7c5996c0b1c4d85f647
Inna Kmit <inna.km7@gmail.com>
2024-11-22 20:28:30 +0000

    task 5

--- js/2-lesson.js
+++ js/2-lesson.js
@@ -78,20 +78,57 @@ console.log(findLongestWord(string)); */
 //яка приймає масив об'єктів і
 //Рядок з назвою каменю.
 //Функція повертає загальну вартість каменів
-//з ​​таким ім'ям, ціною та кількістю з об'єкта
+// //з ​​таким ім'ям, ціною та кількістю з об'єкта
 
-const stones = [
-  { name: "Смарагд", price: 1300, quantity: 4 },
-  { name: "Діамант", price: 2700, quantity: 6 },
-  { name: "Сапфір", price: 400, quantity: 7 },
-  { name: "Щебінь", price: 150, quantity: 100 },
+// const stones = [
+//   { name: "Смарагд", price: 1300, quantity: 4 },
+//   { name: "Діамант", price: 2700, quantity: 6 },
+//   { name: "Сапфір", price: 400, quantity: 7 },
+//   { name: "Щебінь", price: 150, quantity: 100 },
+// ];
+
+// function calcTotal(stones, stonesName) {
+//   for (const stone of stones) {
+//     if (stone.name === stonesName) {
+//       return `Total ${stonesName} = ${stone.price * stone.quantity}`
+//     }
+//   }
+// }
+// console.log('stones :>> ', calcTotal(stones, "Смарагд"));
+
+
+// Угруповання об'єктів студентів по курсам
+// Створіть масив об'єктів, що являють собою різних студентів. Кожен об'єкт повинен містити властивості ім'я, вік та курс.
+// Використовуйте цикл for...of, щоб згрупувати студентів за курсами.
+// Результатом буде об'єкт, де ключами будуть назви курсів, а значення – масиви об'єктів студентів, що належать до відповідного курсу.
+// Очікуванний результат:
+// const groupedStudents = {
+//   Java: [
+//     { name: "Алиса", age: 20, course: "Java" },
+//     { name: "Карл", age: 21, course: "Java" },
+//   ],
+//   Python: [{ name: "Боб", age: 22, course: "Python" }],
+//   JavaScript: [{ name: "Джон", age: 23, course: "JavaScript" }],
+// };
+const students = [
+  { name: "Алиса", age: 20, course: "Java" },
+  { name: "Боб", age: 22, course: "Python" },
+  { name: "Карл", age: 21, course: "Java" },
+  { name: "Джон", age: 23, course: "JavaScript" },
 ];
+function groupedStudents(students) {
+  
+  const allStudents = {};
+  for (let student of students) {
+    console.log(allStudents[student.course]);
 
-function calcTotal(stones, stonesName) {
-  for (const stone of stones) {
-    if (stone.name === stonesName) {
-      return `Total ${stonesName} = ${stone.price * stone.quantity}`
-    }
+    if (!allStudents[student.course]) {
+      allStudents[student.course] = [];
+    };
+    allStudents[student.course].push(student);
+    // console.log(allStudents[student.course]);
+    // console.log(allStudents);
   }
+  return allStudents;
 }
-console.log('stones :>> ', calcTotal(stones, "Смарагд"));
+console.log(groupedStudents(students));
