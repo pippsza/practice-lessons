2148ba866ba92f6dea270a8f006e723644551f47
Andriy Kiselyov <168927164+andrelux-ua@users.noreply.github.com>
2024-12-20 20:51:47 +0200

    task-4-03

--- js/4-lesson.js
+++ js/4-lesson.js
@@ -15,12 +15,38 @@
 Після натискання кнопки "SWAP ME" здійснюється обмін вмістом між двома інпутами.
 Ви можете натиснути на неї кілька разів або вручну змінити вміст інпутів.
 */
-const swapButton = document.querySelector("#swapButton");
-const leftInput = document.querySelector("#leftSwapInput");
-const rightInput = document.querySelector("#rightSwapInput");
-console.log(swapButton, leftInput, rightInput);
-swapButton.addEventListener("click", () => {
-  const swapper = leftInput.value;
-  leftInput.value = rightInput.value;
-  rightInput.value = swapper;
+// const swapButton = document.querySelector("#swapButton");
+// const leftInput = document.querySelector("#leftSwapInput");
+// const rightInput = document.querySelector("#rightSwapInput");
+// console.log(swapButton, leftInput, rightInput);
+// swapButton.addEventListener("click", () => {
+//   const swapper = leftInput.value;
+//   leftInput.value = rightInput.value;
+//   rightInput.value = swapper;
+// });
+
+/* <!-- ЗАДАЧА 3 -->
+    <!-- <div>
+      <p class="taskTitle">ЗАДАЧА 3</p>
+      <input id="passwordInput" type="text" />
+      <button id="passwordButton">Приховати</button>
+    </div> -->
+/*
+Завдання 3
+Кнопка "Приховати" ховає текст і замінює назву кнопки на
+"Розкрити", при повторному натисканні текст знову стає доступним
+і кнопка набуває початкового вигляду.
+ */
+
+const passwordInput = document.querySelector("#passwordInput");
+const passwordButton = document.querySelector("#passwordButton");
+
+passwordButton.addEventListener("click", () => {
+  if (passwordInput.type === "text") {
+    passwordInput.type = "password";
+    passwordButton.textContent = "Розкрити";
+  } else {
+    passwordInput.type = "password";
+    passwordButton.textContent = "Приховати";
+  }
 });
